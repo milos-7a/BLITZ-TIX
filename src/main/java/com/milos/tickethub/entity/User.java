0b94dff72e8e98107f1e51e0d9aa9c8a6591e430
milos-7a <milosnikolic7a@gmail.com>
2026-06-16 20:27:53 +0200

    Integrated roles

--- src/main/java/com/milos/tickethub/entity/User.java
+++ src/main/java/com/milos/tickethub/entity/User.java
@@ -3,7 +3,6 @@ package com.milos.tickethub.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.List;
 import java.util.Set;
 
 @Entity
--- src/main/java/com/milos/tickethub/service/AuthServiceImpl.java
+++ src/main/java/com/milos/tickethub/service/AuthServiceImpl.java
@@ -17,7 +17,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.Set;
 
 @Service
